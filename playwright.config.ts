1e68ed41512df2c1b199892e1746d1eac943f509
thichankem <dophuhung.hn@gmail.com>
2026-07-19 22:49:48 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   retries: 0,
   workers: 1,
   reporter: 'html',
+  timeout: 180_000,
   use: {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
@@ -15,6 +16,13 @@ export default defineConfig({
     viewport: { width: 1280, height: 800 },
   },
   projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',   // Dùng Chrome thật thay vì Chromium của Playwright
+      },
+    },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
